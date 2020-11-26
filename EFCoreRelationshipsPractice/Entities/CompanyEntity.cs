2610c9c32179362e2e48df25da962b1bcfb9443f
Ying Fu <YFu3@slb.com>
2020-11-26 15:47:54 +0800

    feat: add employee

--- EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
@@ -16,6 +16,7 @@ namespace EFCoreRelationshipsPractice.Entities
         {
             this.Name = companyDto.Name;
             this.Profile = new ProfileEntity(companyDto.Profile);
+            this.Employees = companyDto.Employees.Select(employeeDto => new EmployeeEntity(employeeDto)).ToList();
         }
 
         public int Id { get; set; }
@@ -45,6 +46,18 @@ namespace EFCoreRelationshipsPractice.Entities
 
     public class EmployeeEntity
     {
+        private EmployeeDto employeeDto;
+
+        public EmployeeEntity()
+        {
+        }
+
+        public EmployeeEntity(EmployeeDto employeeDto)
+        {
+            this.Name = employeeDto.Name;
+            this.Age = employeeDto.Age;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
