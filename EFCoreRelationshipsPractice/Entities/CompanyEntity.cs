fe11038f88b593362fa93b8f797c9803692b97ef
Ying Fu <YFu3@slb.com>
2020-11-26 15:23:58 +0800

    feat: add profile

--- EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCoreRelationshipsPractice.Dtos;
 
 namespace EFCoreRelationshipsPractice.Entities
 {
     public class CompanyEntity
     {
+        public CompanyEntity()
+        {
+        }
+
+        public CompanyEntity(CompanyDto companyDto)
+        {
+            this.Name = companyDto.Name;
+            this.Profile = new ProfileEntity(companyDto.Profile);
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
@@ -16,6 +27,16 @@ namespace EFCoreRelationshipsPractice.Entities
 
     public class ProfileEntity
     {
+        public ProfileEntity()
+        {
+        }
+
+        public ProfileEntity(ProfileDto profileDto)
+        {
+            this.CertId = profileDto.CertId;
+            this.RegisteredCapital = profileDto.RegisteredCapital;
+        }
+
         public int Id { get; set; }
         public int RegisteredCapital { get; set; }
         public string CertId { get; set; }
