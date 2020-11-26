80fe2cc06b2a5ad1682f27eb183d50212d11394d
Jiankun YANG <JYang53@slb.com>
2020-11-26 14:47:16 +0800

    feat: add location property to company model

--- EFCoreRelationshipsPractice/NewFolder/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/NewFolder/CompanyEntity.cs
@@ -10,5 +10,6 @@ namespace EFCoreRelationshipsPractice.NewFolder
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string Location { get; set; }
     }
 }
