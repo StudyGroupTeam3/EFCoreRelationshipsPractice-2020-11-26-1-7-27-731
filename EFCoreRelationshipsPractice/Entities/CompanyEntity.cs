9c982b1f6b3f5eb0a3c57fad33a7ef8e27c3a857
Ziyuan <zxu25@slb.com>
2020-11-26 13:37:51 +0800

    feat: implemented CompanyService.AddCompany() to add data into db

--- EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
@@ -7,7 +7,7 @@ namespace EFCoreRelationshipsPractice.Entities
 {
     public class CompanyEntity
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
