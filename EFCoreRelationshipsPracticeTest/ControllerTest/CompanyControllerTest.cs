6dcfe790ae4a302058af981e98a85f52b96c8f39
Jiankun YANG <JYang53@slb.com>
2020-11-26 18:55:08 +0800

    test: add test case for CompanyService

--- EFCoreRelationshipsPracticeTest/ControllerTest/CompanyControllerTest.cs
+++ EFCoreRelationshipsPracticeTest/ControllerTest/CompanyControllerTest.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using EFCoreRelationshipsPractice;
 using EFCoreRelationshipsPractice.Dtos;
+using EFCoreRelationshipsPractice.Repository;
+using EFCoreRelationshipsPractice.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -55,6 +58,9 @@ namespace EFCoreRelationshipsPracticeTest
             Assert.Equal(companyDto.Employees[0].Name, returnCompanies[0].Employees[0].Name);
             Assert.Equal(companyDto.Profile.CertId, returnCompanies[0].Profile.CertId);
             Assert.Equal(companyDto.Profile.RegisteredCapital, returnCompanies[0].Profile.RegisteredCapital);
+            //var scope = Factory.Services.CreateScope();
+            //var scopedServices = scope.ServiceProvider;
+            //var context = scopedServices.GetRequiredService<CompanyDbContext>();
         }
 
         [Fact]
@@ -123,6 +129,37 @@ namespace EFCoreRelationshipsPracticeTest
             var returnCompanies = JsonConvert.DeserializeObject<List<CompanyDto>>(body);
 
             Assert.Equal(2, returnCompanies.Count);
+        }
+
+        [Fact]
+        public async Task Should_create_company_succes_via_company_service()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+
+            CompanyDbContext context = scopedServices.GetRequiredService<CompanyDbContext>();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.Employees = new List<EmployeeDto>()
+            {
+                new EmployeeDto()
+                {
+                    Name = "Tom",
+                    Age = 19
+                }
+            };
+
+            companyDto.Profile = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
+
+            CompanyService companyService = new CompanyService(context);
+
+            await companyService.AddCompany(companyDto);
+
+            Assert.Equal(1, context.Companies.Count());
         }
     }
 }
