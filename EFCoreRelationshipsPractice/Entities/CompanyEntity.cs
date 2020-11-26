44b5acabf250878aa6075546e02dae44df4054a2
Ziyuan <zxu25@slb.com>
2020-11-26 15:20:21 +0800

    feat: complete function to add company entity with profile into db

--- EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
@@ -8,6 +8,16 @@ namespace EFCoreRelationshipsPractice.Entities
 {
     public class CompanyEntity
     {
+        public CompanyEntity()
+        {
+        }
+
+        public CompanyEntity(CompanyDto companyDto)
+        {
+            this.Name = companyDto.Name;
+            this.Profile = new ProfileEntity(companyDto.Profile);
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public ProfileEntity Profile { get; set; }
@@ -15,6 +25,16 @@ namespace EFCoreRelationshipsPractice.Entities
 
     public class ProfileEntity
     {
+        public ProfileEntity()
+        {
+        }
+
+        public ProfileEntity(ProfileDto profileDto)
+        {
+            this.CertId = profileDto.CertId;
+            this.RegisteredCapital = profileDto.RegisteredCapital;
+        }
+
         public int Id { get; set; }
         public int RegisteredCapital { get; set; }
         public string CertId { get; set; }
